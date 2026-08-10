205fb100077e56dd6e5f6b6a8a9bbd2004b1095d
saad80201 <snadeeb02@gmail.com>
2026-08-11 00:55:43 +0600

    Fixed CORS Configuration

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,19 @@ const chatRoutes = require('./routes/chatRoutes');
 const app = express();
 
 // CORS for Vue frontend's access to the backend
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:5173'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST'],
   credentials: true
 }));
